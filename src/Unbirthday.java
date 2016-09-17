c3bf6ae7f825c30752d8f903d5e5db92dfe17b8b
Oliver-Nizet <olivernizet@att.net>
2016-09-17 09:48:06 -0700

    Date change

--- src/Unbirthday.java
+++ src/Unbirthday.java
@@ -2,7 +2,7 @@ import javax.swing.JOptionPane;
 
 public class Unbirthday {
 	public static void main(String[] args) {
-		String today = "September 13th";
+		String today = "September 17th";
 		String birthday = JOptionPane.showInputDialog("What is your birthday");
 		if (birthday.equals(today)) {
 			JOptionPane.showMessageDialog(null, "HAPPY BIRTHDAY! Your birthday is " + birthday + "!");
