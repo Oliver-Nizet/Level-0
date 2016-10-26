f39b3dbb678077139d5bc42412c8bfd38362b098
rylr <league@WTS9.attlocal.net>
2016-10-25 18:18:48 -0700

    pug life

---
+++ src/PlayingWithIcons.java
@@ -0,0 +1,14 @@
+import javax.swing.ImageIcon;
+import javax.swing.JOptionPane;
+
+public class PlayingWithIcons {
+	public static void main(String[] args) {
+		String myMessage = JOptionPane.showInputDialog("What do you want your message to be?");
+		String myTitle = JOptionPane.showInputDialog("What do you want your title to be?");
+		JOptionPane.showMessageDialog(null, myMessage, myTitle, 0, getIcon());
+	}
+
+	static ImageIcon getIcon() {
+		return new ImageIcon("image/images.jpg");
+	}
+}
