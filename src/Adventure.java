4c8255a5998a55932f4f6d66c2f7c773e6ae4d04
rylr <league@WTS9.attlocal.net>
2016-12-13 18:23:09 -0800

    bedfast

--- src/Adventure.java
+++ src/Adventure.java
@@ -5,15 +5,22 @@ public class Adventure {
 		int x = 0;
 		int y = 0;
 		Scanner scanner = new Scanner(System.in);
-		System.out.println("Welcome to your Adventure. You are in a forest. What should you do?");
+		System.out.println("Welcome to your Adventure. Give me commands to go north, south, east, or west.\nYour cordinates are: 0,0");
 		while (true) {
 			String line = scanner.nextLine();
-			if (line.equals("look down")) {
-				System.out.println("There is a paper.");
+			if (line.equals("go north")) {
+				y++;
 			}
-			if (line.equals("read paper")) {
-				System.out.println("It sais: If you go west you will find the next clue.");
+			if (line.equals("go south")) {
+				y--;
 			}
+			if (line.equals("go east")) {
+				x++;
+			}
+			if (line.equals("go west")) {
+				x--;
+			}
+			System.out.println("Your cordinates are: " + x + "," + y);
 		}
 	}
 }
