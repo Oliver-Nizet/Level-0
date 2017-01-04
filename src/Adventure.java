d8a086fe907f6fc32a28e12994bc05d605457534
DJA526 <league@WTS13.attlocal.net>
2017-01-03 18:25:15 -0800

    hi

--- src/Adventure.java
+++ src/Adventure.java
@@ -23,9 +23,11 @@ public class Adventure {
 			System.out.println("Your coordinates are: " + x + "," + y);
 
 			if (line.equals("go north") || line.equals("go south") || line.equals("go east") || line.equals("go west")) {
-				System.out.println("Now you know how to move around. Go to the coordinates are");
+				System.out.println("Now you know how to move around. Go to the coordinates 2,3");
 			}
-
+			if (x == 2 && y == 3) {
+				System.out.println("You found a locked chest on the ground. There is a slip of paper on it that says that the key is at the coordinates -6, 4");
+			}
 		}
 	}
 }
